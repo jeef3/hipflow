025b394ee06b26d654a367f736175573900d502d
Jeff Knaggs <mail@jeef3.com>
2014-02-06 23:20:41 +1300

    Fixed typo in project name

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('hipFlowAApp')
-  .controller('MainCtrl', function ($scope) {
+angular.module('hipFlowApp')
+  .controller('MainCtrl', function ($scope, Flowdock) {
     $scope.rooms = [
       { name: 'Idle chit-chat' },
       { name: 'Dev' }
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('hipFlowAApp'));
+  beforeEach(module('hipFlowApp'));
 
   var MainCtrl,
     scope;
