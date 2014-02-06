527155ca12d2e25ba95c46a7c6b320b6c7856333
Jeff Knaggs <mail@jeef3.com>
2014-02-06 23:07:21 +1300

    Fleshing out some ideas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,27 @@
 
 angular.module('hipFlowAApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.rooms = [
+      { name: 'Idle chit-chat' },
+      { name: 'Dev' }
     ];
+
+    $scope.queries = [
+      { name: 'Alan', online: true },
+      { name: 'Serge', online: false }
+    ];
+
+    $scope.messages = [
+      {
+        user: 'Jeff Knaggs',
+        content: 'Maybe have screenshots of situation/options for context?\nnearly every page will have a learner search. Should it be a right-side filter?'
+      }
+    ];
+
+    $scope.send = function (message) {
+      $scope.messages.push({
+        user: 'Jeff Knaggs',
+        content: message
+      });
+    };
   });
