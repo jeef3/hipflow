7c9d3e85790731593e17921eec8bcbab98da853e
Jeff Knaggs <mail@jeef3.com>
2014-02-10 22:04:22 +1300

    Added GitHub commit comments

--- app/scripts/filters/short-hash.js
+++ app/scripts/filters/short-hash.js
@@ -3,6 +3,6 @@
 angular.module('hipFlowApp')
   .filter('shortHash', function () {
     return function (input) {
-      return input.substring(0, 7);
+      return input ? input.substring(0, 7) : input;
     };
   });
