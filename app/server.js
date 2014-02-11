0feac440558be8879941609575421b51a489752a
Jeff Knaggs <mail@jeef3.com>
2014-02-12 01:01:49 +1300

    Very basic static asset serving

--- app/server.js
+++ app/server.js
@@ -4,6 +4,8 @@ var express = require('express'),
   passport = require('passport'),
   app = express();
 
+app.set('port', 9000);
+
 app.configure('development', function () {
 
 });
@@ -19,6 +21,16 @@ app.get('/login', passport.authenticate('flowdock'));
 app.get('/oauth/callback',
   passport.authenticate('flowdock', { successRedirect: '/',
                                       failureRedirect: '/login' }));
+
+app.use('/images', express.static('app/images'));
+app.use('/scripts', express.static('app/scripts'));
+app.use('/styles', express.static('.tmp/styles'));
+app.use('/views', express.static('app/views'));
+app.use('/bower_components', express.static('app/bower_components'));
+
+app.get('/', function (req, res) {
+  res.sendfile('app/index.html');
+});
 
 app.listen(app.get('port'), function () {
   console.log('Listening on %d', app.get('port'));
