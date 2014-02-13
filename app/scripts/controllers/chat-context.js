bfa98616527141e0fde8159d80081f6cfc404cb0
Jeff Knaggs <mail@jeef3.com>
2014-02-13 23:37:01 +1300

    Some style ideas for discussion activity

--- app/scripts/controllers/chat-context.js
+++ app/scripts/controllers/chat-context.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('hipFlowApp')
-  .controller('ChatContextCtrl', function ($scope, Flowdock) {
-
+  .controller('ChatContextCtrl', function ($scope) {
+    $scope.discussions = [
+      { title: 'What happened to all the other uses of UrlUtil.GetExternalUrl() ?', muted: true },
+      { title: 'Linux gurus ideas easiest way to do this:', muted: false },
+      { title: 'I converted INT over to same issue & workflow schemes as the other current projects. Now bugs can be put in testing column!', muted: true }
+    ];
   });
