7d44c97d4d0a0c9b4c8aa2763ad550a9ca1d194c
Jeff Knaggs <mail@jeef3.com>
2014-02-16 09:12:08 +1300

    Some chat input styling

--- app/scripts/filters/code-format.js
+++ app/scripts/filters/code-format.js
@@ -3,7 +3,7 @@
 angular.module('hipFlowApp')
   .filter('codeFormat', function () {
     return function (input) {
-      if (input.match(/^    /)) {
+      if (input && input.match(/^    /)) {
         return '<code><pre>' + input + '</pre></code>';
       } else {
         return input;
