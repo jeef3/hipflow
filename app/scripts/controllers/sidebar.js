8dff4028b8749b0afd5d5efbf8ca24390582c778
Jeff Knaggs <mail@jeef3.com>
2014-02-16 22:49:44 +1300

    Stop notification marker appearing when not needed

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -5,12 +5,18 @@ angular.module('hipFlowApp')
     $scope.$on('NEW_MESSAGE', function (e, message) {
       var messageRoomId;
 
+      if (message.user === Flowdock.me().id) {
+        return;
+      }
+
       if (message.flow) {
         messageRoomId = message.flow;
       } else if (message.to) {
         messageRoomId = message.to === Flowdock.me().id ?
           message.user :
           message.to;
+
+        messageRoomId = parseInt(messageRoomId, 10);
       }
 
       if (messageRoomId !== $scope.currentRoom.id) {
