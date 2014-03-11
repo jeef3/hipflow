963e355b1c002ac12db7dcc0bbf718d87233f133
Jeff Knaggs <mail@jeef3.com>
2014-03-12 09:18:01 +1300

    Fixed more-content spec

--- test/spec/directives/more-content.js
+++ test/spec/directives/more-content.js
@@ -15,6 +15,6 @@ describe('Directive: moreContent', function () {
   it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<more-content></more-content>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the moreContent directive');
+    expect(element.text()).toBe('');
   }));
 });
