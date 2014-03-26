171452f0956a2b969a09f3db8612b3f0ae92df27
Jeff Knaggs <mail@jeef3.com>
2014-03-26 22:46:12 +1300

    Use dotenv

--- app/config/config.js
+++ app/config/config.js
@@ -1,12 +1,10 @@
 'use strict';
 
-var path = require('path');
-
 var nconf = require('nconf');
+require('dotenv').load();
 
 nconf.argv()
   .env()
-  .file({ file: path.join(__dirname, 'env.json') })
   .defaults({ 'PORT': 9000 });
 
 module.exports = nconf;
