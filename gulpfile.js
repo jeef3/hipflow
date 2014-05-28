e85f5c79379dcc952e9143be35773a395d3b6298
Jeff Knaggs <mail@jeef3.com>
2014-05-28 21:19:13 +1200

    Simple conversion to stylus

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,16 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-  return gulp.src('app/styles/styles.scss')
+  return gulp.src('app/styles/styles.styl')
     // .pipe($.sass())
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10,
-      sourcemap: true,
-      trace: true,
-      lineNumbers: true
-    }))
+    // .pipe($.rubySass({
+    //   style: 'expanded',
+    //   precision: 10,
+    //   sourcemap: true,
+    //   trace: true,
+    //   lineNumbers: true
+    // }))
+    .pipe($.stylus())
     .pipe($.autoprefixer('last 1 version'))
     .pipe(gulp.dest('.tmp/styles'))
     .pipe($.size());
@@ -109,7 +110,7 @@ gulp.task('default', ['clean'], function () {
 gulp.task('wiredep', function () {
   var wiredep = require('wiredep').stream;
 
-  gulp.src('app/styles/*.scss')
+  gulp.src('app/styles/*.styl')
     .pipe(wiredep({ directory: 'app/bower_components' }))
     .pipe(gulp.dest('app/styles'));
 
@@ -139,7 +140,7 @@ gulp.task('watch', ['serve'], function () {
     server.changed(file.path);
   });
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/styles/**/*.styl', ['styles']);
   // gulp.watch('app/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
 });
