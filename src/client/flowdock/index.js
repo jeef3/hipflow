57cc24efa1eb1568ecc9c6932fb5514d959058ad
Jeff Knaggs <mail@jeef3.com>
2015-03-07 14:31:34 +1300

    Fix flowdock connector

--- src/client/flowdock/index.js
+++ src/client/flowdock/index.js
@@ -2,8 +2,6 @@
 
 import Flowdock from './flowdock';
 import FlowdockAuth from './flowdock-auth';
-import FlowdockConnector from './flowdock-connector';
 
 export {FlowdockAuth};
-export {FlowdockConnector};
 export default Flowdock;
