db138dfe6b5b1915a4ae88b9e653ac165dcc9b0e
Jeff Knaggs <mail@jeef3.com>
2015-03-11 22:24:46 +1300

    Adding chat bar back in

--- src/client/components/ChatBar.react.js
+++ src/client/components/ChatBar.react.js
@@ -2,29 +2,32 @@
 
 import React from 'react';
 
+import Icon from './Icon.react';
+import Button from './Button.react';
+
 class ChatBar extends React.Component {
   render() {
     return (
-      <div className="chat-bar">
-        <div className="current-discussion" ng-show="currentDiscussion.id">
-          <i className="fa fa-fw fa-angle-double-right"></i>
+      <div className="c-ChatBar">
+        <div className="c-ChatBar__CurrentDiscussion" ng-show="currentDiscussion.id">
+          <Icon kind="angle-double-right" />
           <span ng-bind-html="currentDiscussion.title | emoji"></span>
-          <button className="btn close" ng-click="setCurrentDiscussion()">
-            <i className="fa fa-fw fa-times"></i>
-          </button>
+          <Button className="close" ng-click="setCurrentDiscussion()">
+            <Icon kind="times" />
+          </Button>
         </div>
         <form name="sendMessageForm" ng-submit="send(message)">
-          <div className="chat-input-container">
+          <div className="c-ChatBar__InputContainer">
             <textarea
                 chat-input-submit="send(message)"
                 focus-on="CURRENT_DISCUSSION_SET"
-                placeholder="Chat..."
-                className="chat-input"
+                placeholder="Type to chat..."
+                className="c-ChatBar__Input"
                 ng-model="message"></textarea>
           </div>
         </form>
 
-        <p className="user-typing emphasis small quiet" ng-show="usersTyping().length">
+        <p className="c-ChatBar__UserTyping emphasis small quiet" ng-show="usersTyping().length">
           <div
             count="usersTyping().length"
             offset="2"
